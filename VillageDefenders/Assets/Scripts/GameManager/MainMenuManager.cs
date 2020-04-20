f3bdcd7676470e085aa0845652140e9ff0dd3225
Tadas Laurinaitis <tadas.laurinaitis100@gmail.com>
2020-04-20 23:57:38 +0300

    Sub menus, lots of textures
    * Added sub-menus to main menu
    * Added lots of new textures for further use

--- VillageDefenders/Assets/Scripts/GameManager/MainMenuManager.cs
+++ VillageDefenders/Assets/Scripts/GameManager/MainMenuManager.cs
@@ -1,10 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+    private GameObject mainMenu;
+    private GameObject settingsMenu;
+    private GameObject aboutMenu;
+
+    void Awake()
+    {
+        mainMenu = transform.Find("MainMenu").gameObject;
+        settingsMenu = transform.Find("SettingsMenu").gameObject;
+        aboutMenu = transform.Find("AboutMenu").gameObject;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +32,56 @@ public class MainMenuManager : MonoBehaviour
     public void OnPlayButtonClick()
     {
         SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+
+    public void OnQuitButtonClick()
+    {
+        //TODO "Are you sure? Y/N"
+        Application.Quit(0);
+    }
+
+    public void OnSettingsButtonClick()
+    {
+        mainMenu.SetActive(false);
+        //List<Button> buttons = mainMenu.GetComponents<Button>().ToList();
+        //foreach (var b in buttons)
+        //{
+        //    b.animationTriggers.normalTrigger.
+        //}
+        settingsMenu.SetActive(true);
+        ResetCertainMenuButtons(settingsMenu);
+    }
+
+    public void OnAboutButtonClick()
+    {
+        mainMenu.SetActive(false);
+        aboutMenu.SetActive(true);
+        ResetCertainMenuButtons(aboutMenu);
+    }
+
+    public void OnSettingsBackButtonClick()
+    {
+        settingsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+        ResetCertainMenuButtons(mainMenu);
+    }
+
+    public void OnAboutBackButtonClick()
+    {
+        aboutMenu.SetActive(false);
+        mainMenu.SetActive(true);
+        ResetCertainMenuButtons(mainMenu);
+    }
+
+    private void ResetCertainMenuButtons(GameObject menu)
+    {
+        List<Button> buttons = menu.GetComponentsInChildren<Button>().ToList();
+        foreach (var b in buttons)
+        {
+            //b.enabled = true;
+            //b.enabled = false;
+            //b.enabled = true;
+            Debug.Log("reseting " +b);
+        }
     }
 }
